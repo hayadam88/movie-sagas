0a43fa1b2718d1318fd5c2930e32abcbf2b4df6b
Adam Hay <heyadam66@gmail.com>
2019-07-27 15:23:49 -0500

    Set up some server stuff

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const pool = require('./modules/pool');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.get('/api/movies', (req, res) => {
+    pool.query('')
+    res.sendStatus(200);
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
